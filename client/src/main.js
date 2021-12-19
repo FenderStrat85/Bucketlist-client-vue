9f16762a5d856d34c4afe40a4f1a9ab753a3c189
Richard Barnes <rbarnes2@hotmail.co.uk>
2021-12-19 17:05:46 +0000

    vue composable added as vuecli for apollo does not work for vue 3

--- client/src/main.js
+++ client/src/main.js
@@ -3,4 +3,25 @@ import App from './App.vue';
 import router from './router';
 import store from './store/index';
 
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client/core';
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:4000',
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
 createApp(App).use(router).use(store).mount('#app');
