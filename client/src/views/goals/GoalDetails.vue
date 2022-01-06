6a909b795636c7b15d1b10f8e3115326a50ae1e3
Richard Barnes <rbarnes2@hotmail.co.uk>
2022-01-06 17:58:07 +0000

    data now loading correctly and error removed from Goal view. GoalDetails view now rendering different titles dynamically, will need to change to specific components.

--- client/src/views/goals/GoalDetails.vue
+++ client/src/views/goals/GoalDetails.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
-    <h1>I am the the item detail</h1>
-    <p>The item's id is {{ id }}</p>
+    <div v-if="category === 'Travel'">
+      <h1>I am the the {{ category }} detail</h1>
+      <p>The items id is {{ id }}</p>
+      <p>The items category is {{ category }}</p>
+      <p>The items title is {{ title }}</p>
+    </div>
+    <div v-if="category === 'Education'">
+      <h1>I am the the {{ category }} detail</h1>
+      <p>The items id is {{ id }}</p>
+      <p>The items category is {{ category }}</p>
+      <p>The items title is {{ title }}</p>
+    </div>
+    <div v-if="category === 'Personal'">
+      <h1>I am the the {{ category }} detail</h1>
+      <p>The items id is {{ id }}</p>
+      <p>The items category is {{ category }}</p>
+      <p>The items title is {{ title }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 //passing the id through props as opposed to route params
+//passed as params in router link but then accessed as props
 export default {
-  props: ['id'],
+  props: ['id', 'title', 'category'],
   data() {
     return {
-      idFromParam: this.$route.params.id,
+      // idFromParam: this.$route.params.id,
     };
   },
 };
--- client/src/views/goals/Goals.vue
+++ client/src/views/goals/Goals.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
     <h1>I am the item list page</h1>
-    <div v-for="item in result.getBucketListItems" :key="item._id">
-      <router-link :to="{ name: 'GoalDetails', params: { id: item._id } }">
-        <h2>{{ item.title }}</h2>
-      </router-link>
+    <div v-if="result">
+      <div v-for="item in result.getBucketListItems" :key="item._id">
+        <router-link
+          :to="{
+            name: 'GoalDetails',
+            params: {
+              id: item._id,
+              title: item.title,
+              category: item.category,
+            },
+          }"
+        >
+          <h2>{{ item.title }}</h2>
+        </router-link>
+      </div>
     </div>
 
     <div v-if="loading">Loading...</div>
@@ -19,11 +30,6 @@ import gql from 'graphql-tag';
 
 export default {
   setup() {
-    const items = [
-      { title: 'Visit Japan', id: 1 },
-      { title: 'Visit China', id: 2 },
-      { title: 'Visit Korea', id: 3 },
-    ];
     const { result, loading, error } = useQuery(gql`
       query getBucketListItems {
         getBucketListItems {
@@ -46,16 +52,12 @@ export default {
         }
       }
     `);
-    console.log('result', result);
-    console.log('result.value', result.value);
-    const data = result.value;
-    console.log('data', data);
+    // console.log('result', result);
+    // console.log('result.value', result.value);
     return {
-      items,
       result,
       loading,
       error,
-      data,
     };
   },
 };
